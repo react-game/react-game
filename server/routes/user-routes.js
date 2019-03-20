4452beb5b57fe5023fc6f63a1dce38cb359a9147
Amy Knudson <amylknudson@gmail.com>
2019-03-19 23:47:56 -0600

    ak - server routes and models set up

---
+++ server/routes/user-routes.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const userRoute = express.Router()
+const Users = require("./../models/user-model")
+
+userRoute.route("/")
+    .get((req, res) => {
+        Users.find((err, users) => {
+            if(err) return res.status(500).send(err)
+            return res.status(200).send(users)
+        })
+    })
+
+    .post((req, res, next) => {
+        let newUser = new Users(req.body)
+        newUser.save().then(savedUser => res.status(201).send(savedUser)).catch(err => {
+            res.status(500)
+            next(err)
+        })
+    })
+
+userRoute.route("/:_id")
+    .put((req,res) => {
+        Users.findOneAndUpdate({_id: req.params._id}, req.body, {new: true}, (err, user) => {
+            if(err) return res.status(500).send(err)
+            return res.status(200).send(user)
+        } )
+    })
+
+module.exports = userRoute
