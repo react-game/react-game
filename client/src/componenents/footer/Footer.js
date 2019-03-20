b1bd94d8f30ca54a2b1a5779db0a4880317207ee
Lara Potjewyd <larapotjewyd@gmail.com>
2019-03-20 10:27:17 -0600

    added links to footer and changed some styles

--- client/src/componenents/footer/Footer.js
+++ client/src/componenents/footer/Footer.js
@@ -5,9 +5,9 @@ class Footer extends Component {
     render() {
         return (
             <footer>
-                <h2>AMY KNUDSEN</h2>
-                <h2>JESSIE JONES</h2>
-                <h2>LARA POTJEWYD</h2>
+                <h2> <a href="https://www.linkedin.com/in/amy-knudson/" target="_blank"> AMY KNUDSON </a></h2>
+                <h2> <a href="https://www.linkedin.com/in/the-real-jessie-jones/" target="_blank"> JESSIE JONES </a></h2>
+                <h2> <a href="https://www.linkedin.com/in/lara-potjewyd/" target="_blank"> LARA POTJEWYD </a> </h2>
             </footer>
         );
     }
--- client/src/componenents/landing/Landing.js
+++ client/src/componenents/landing/Landing.js
@@ -4,8 +4,14 @@ import './landing.css';
 class Landing extends Component {
   render() {
     return (
-      <div>
-        <button onClick={() => this.props.history.push("/game")}>enter</button>
+      <div className="landing">
+        <h1>THIS IS OUR AMAZING GAME!</h1>
+        <h1>CLICK TO ENTER!</h1>
+
+        {/* please don't put anything inside this "landing-button" div!  */}
+        <div id="landing-button">
+          <h2 onClick={() => this.props.history.push("/game")}>ENTER</h2>
+        </div>
       </div>
     );
   }
