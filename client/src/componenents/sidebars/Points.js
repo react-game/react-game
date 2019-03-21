98fd6759664296dbcccceb3f417571b07254a3c5
Lara Potjewyd <larapotjewyd@gmail.com>
2019-03-20 21:46:55 -0600

    select user, changed styling, added user in canvas

--- client/src/componenents/sidebars/Points.js
+++ client/src/componenents/sidebars/Points.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './points.css'
+import { withInterfaceStore } from '../../shared/InterfaceStore';
 
 class Points extends Component {
   render() {
+    console.log(this.props.user.username)
     return (
       <div className="points-box">
+        <h2>YOU ARE {this.props.user.username}!</h2>
         <h2>POINTS:</h2>
       </div>
     );
   }
 }
 
-export default Points;
+export default withInterfaceStore(Points);
