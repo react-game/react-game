ab5b3aee93f2a56e9d0064261d380d2f71ddf4bc
Jessie Jones <jessiejonesofficial@gmail.com>
2019-03-25 19:18:03 -0600

    Made contact info more visible

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -5,9 +5,10 @@ class Footer extends Component {
     render() {
         return (
             <footer>
-                <h2> <a href="https://www.linkedin.com/in/amy-knudson/" target="_blank" rel="noopener noreferrer"> AMY KNUDSON </a></h2>
-                <h2> <a href="https://www.linkedin.com/in/the-real-jessie-jones/" target="_blank" rel="noopener noreferrer"> JESSIE JONES </a></h2>
-                <h2> <a href="https://www.linkedin.com/in/lara-potjewyd/" target="_blank" rel="noopener noreferrer"> LARA POTJEWYD </a> </h2>
+                <h2>CONTACT US: </h2>
+                <h2 className="links"> <a href="https://www.linkedin.com/in/amy-knudson/" target="_blank" rel="noopener noreferrer"> AMY KNUDSON </a></h2>
+                <h2 className="links"> <a href="https://www.linkedin.com/in/the-real-jessie-jones/" target="_blank" rel="noopener noreferrer"> JESSIE JONES </a></h2>
+                <h2 className="links"> <a href="https://www.linkedin.com/in/lara-potjewyd/" target="_blank" rel="noopener noreferrer"> LARA POTJEWYD </a> </h2>
             </footer>
         );
     }
