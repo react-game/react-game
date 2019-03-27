7a24be9357cbd676994bb5db3e45fd0a79fa935a
Lara Potjewyd <larapotjewyd@gmail.com>
2019-03-27 08:35:52 -0600

    changed file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
-import Media from 'react-media';
 import GameDisplay from './components/gameDisplay/GameDisplay';
 import Footer from './components/footer/Footer';
 import Landing from './components/landing/Landing';
 import DeathPage from './components/deathPage/DeathPage';
 
 const App = () => {
+  console.log(navigator.userAgent)
+  console.log(!navigator.userAgent.match(/Android|Mobile|iPhone|iPad|iPod|BlackBerry|Opera Mini|IEMobile/ig))
+
   return (
     <div>
-    <Media query="(max-width: 1023px)">
-          {matches =>
-            matches ? (
+          {navigator.userAgent.match(/Android|Mobile|iPhone|iPad|iPod|BlackBerry|Opera Mini|IEMobile/ig) ? (
               <div className="app">
                 <p>Please use a desktop computer to play our game, as it requires a keyboard.</p>
                 <img src="https://robohash.org/flufferkins?set=set4" alt="cat"/>
@@ -30,8 +30,6 @@ const App = () => {
               </div>
             )
           }
-        </Media>
-    
     </div>
   );  
 };
